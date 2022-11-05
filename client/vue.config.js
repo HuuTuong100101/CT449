bf6789858dcce27cff97cb9ce09382b2c90f1580
HuuTuong100101 <tuongb1910480@student.ctu.edu.vn>
2022-11-05 10:39:44 +0700

    Lam lai phan Client

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,9 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer: {
-    proxy: "http://localhost:3000"
-  }
+  transpileDependencies: true
 })
